27016349a96f9dd70e141e0294217ad23ce37e86
Nareshkumar Rao <contact@nareshkumarrao.com>
2024-06-16 01:19:41 +0200

    run some cleanup

--- src/trojan/server/mod.rs
+++ src/trojan/server/mod.rs
@@ -9,14 +9,14 @@ use tokio::net::TcpListener;
 use crate::{
     config::{LoadFromToml, ServerConfig},
     dns::DnsResolver,
-    tls::{certificates::Certificates, io::TlsAdapters},
+    tls::{certificates::Certificates, io::get_tls_acceptor},
 };
 
 pub async fn server_main() -> anyhow::Result<()> {
     let server_config = ServerConfig::load(Path::new("samples/server.toml"))?;
     debug!("Loaded configs, ready to listen.");
 
-    let tls_adapters = TlsAdapters::new(Certificates::load(&server_config)?)?;
+    let tls_acceptor = get_tls_acceptor(Certificates::load(&server_config)?)?;
     let dns_resolver = Arc::new(DnsResolver::new().await);
 
     let listener = TcpListener::bind(&server_config.listen_addr).await?;
@@ -25,20 +25,12 @@ pub async fn server_main() -> anyhow::Result<()> {
         debug!("Incoming socket");
 
         let server_config = server_config.clone();
-        let tls_adapters = tls_adapters.clone();
+        let tls_acceptor = tls_acceptor.clone();
         let dns_resolver = dns_resolver.clone();
 
         tokio::spawn(async move {
-            let result = match tls_adapters.acceptor.accept(tcp_stream).await {
-                Ok(tls_stream) => {
-                    handle_socket(
-                        &dns_resolver,
-                        &server_config,
-                        &tls_adapters.connector,
-                        tls_stream,
-                    )
-                    .await
-                }
+            let result = match tls_acceptor.accept(tcp_stream).await {
+                Ok(tls_stream) => handle_socket(&dns_resolver, &server_config, tls_stream).await,
                 Err(e) => Err(e.into()),
             };
             if let Err(e) = result {
