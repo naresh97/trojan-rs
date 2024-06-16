35595ef401799a4c4310449868fea697e095005b
Nareshkumar Rao <629990+naresh97@users.noreply.github.com>
2024-06-16 04:04:43 +0200

    add self signing test for debugging

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -15,3 +15,19 @@ pub trait LoadFromToml {
         Ok(config)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn default_config() {
+        let client = ClientConfig::default();
+        let client = toml::to_string_pretty(&client).unwrap();
+        std::fs::write("samples/client.toml", client).unwrap();
+
+        let server = ServerConfig::default();
+        let server = toml::to_string_pretty(&server).unwrap();
+        std::fs::write("samples/server.toml", server).unwrap();
+    }
+}
--- src/tls/io.rs
+++ src/tls/io.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use anyhow::Result;
+use rustls::pki_types::CertificateDer;
 use tokio_rustls::{TlsAcceptor, TlsConnector};
 
 use super::certificates::Certificates;
@@ -13,9 +14,15 @@ pub struct TlsAdapters {
 
 impl TlsAdapters {
     pub fn new(certificates: Certificates<'static>) -> Result<TlsAdapters> {
+        let self_signed = if cfg!(debug_assertions) {
+            certificates.cert.first().cloned()
+        } else {
+            None
+        };
+
         Ok(TlsAdapters {
             acceptor: get_tls_acceptor(certificates)?,
-            connector: get_tls_connector(),
+            connector: get_tls_connector(self_signed)?,
         })
     }
 }
@@ -23,16 +30,27 @@ impl TlsAdapters {
 pub fn get_tls_acceptor(certificates: Certificates<'static>) -> Result<TlsAcceptor> {
     let config = rustls::ServerConfig::builder()
         .with_no_client_auth()
-        .with_single_cert(certificates.cert, certificates.private_key)?;
+        .with_single_cert(
+            certificates.cert.clone(),
+            certificates.private_key.clone_key(),
+        )?;
     let acceptor = TlsAcceptor::from(Arc::new(config));
     Ok(acceptor)
 }
 
-pub fn get_tls_connector() -> TlsConnector {
-    let root_store =
+pub fn get_tls_connector(self_signed: Option<CertificateDer<'static>>) -> Result<TlsConnector> {
+    let mut root_store =
         rustls::RootCertStore::from_iter(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
+
+    if cfg!(debug_assertions) {
+        // Allow the use of self-signed certificates in the debug builds to test
+        if let Some(self_signed) = self_signed {
+            root_store.add(self_signed)?;
+        }
+    }
+
     let config = rustls::ClientConfig::builder()
         .with_root_certificates(root_store)
         .with_no_client_auth();
-    TlsConnector::from(Arc::new(config))
+    Ok(TlsConnector::from(Arc::new(config)))
 }
